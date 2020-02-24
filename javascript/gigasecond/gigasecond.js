0d3966d05404ec8d295e44eb7b11ad5ee056f398
Tayla Monet <taymjenk@gmail.com>
2020-02-24 11:49:40 -0500

    gigasecond solution

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
+export const gigasecond = time => {
+   //convert input time to milliseconds from January 1, 1970
+   const milliseconds = time.getTime()
+   //add one gigasecond to the current milliseconds
+   // one gigasecond = 10^12 milliseconds
+   const plusGigaSec = milliseconds + Math.pow(10, 12)
+   console.log(time)
+   return new Date(plusGigaSec)
 };
