c7efb9c396def29cac43562ec5970801c701f150
Tayla Monet <taymjenk@gmail.com>
2020-02-29 22:48:00 -0500

    npm i & soulution

---
+++ javascript/rna-transcription/rna-transcription.js
@@ -0,0 +1,17 @@
+//
+// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+const COMPLEMENT = {
+  G: "C",
+  C: "G",
+  T: "A",
+  A: "U"
+}
+
+export const toRna = dna => {
+  if (!dna) return ''
+  return dna.split('').map(nucleo => {
+    return nucleo = COMPLEMENT[nucleo]
+  }).join('')
+}
