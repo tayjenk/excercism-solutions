692a38feef7279334076eab22b5c33eb87b3f92b
Tayla Monet <taymjenk@gmail.com>
2020-03-15 18:36:28 -0400

    new solution, func for letter check and alpha size

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,12 +2,15 @@
 // This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const ENG_ALPHABET_SIZE = 26
+
+const inAlphabet = (char) => char >= 'a' && char <= 'z'
 
 export const isPangram = (sentence) => {
   if (!sentence) return false
-  const alpha = new Set()
-  for(let letter of sentence.toLowerCase()) {
-    if(letter >= 'a' && letter <= 'z') alpha.add(letter)
+  const letters = new Set()
+  for(let char of sentence.toLowerCase()) {
+    if(inAlphabet(char)) letters.add(char)
   }
-  return alpha.size === 26
+  return letters.size === ENG_ALPHABET_SIZE
 };
