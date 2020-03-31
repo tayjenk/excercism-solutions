25d3f8aeff38fa22ce1a0f4a595e1ea9003c1d6e
Tayla Monet <taymjenk@gmail.com>
2020-03-31 19:30:08 -0400

    added helper functions

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,21 +3,28 @@
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const isSilent = message => RegExp(/^\''|^\s+$/).test(message)
+const isYellingAsking = message => message === message.toUpperCase() && RegExp(/[A-Z]\?/).test(message)
+const isAsking = message => RegExp(/\?$|(\?\s+)$/).test(message)
+const isYelling = message => message === message.toUpperCase() && RegExp(/[A-Z]/).test(message)
+
 
 export const hey = message => {
   let response = "Whatever."
   switch (!!message) {
-    case RegExp(/^\''|^\s+$/).test(message):
+    case isSilent(message):
       response = "Fine. Be that way!"
       break
-    case message === message.toUpperCase() && RegExp(/[A-Z]\?/).test(message):
+    case isYellingAsking(message):
       response = "Calm down, I know what I'm doing!"
       break
-    case RegExp(/\?$|(\?\s+)$/).test(message):
+    case isAsking(message):
       response = "Sure."
       break
-    case message === message.toUpperCase() && RegExp(/[A-Z]/).test(message):
+    case isYelling(message):
       response = "Whoa, chill out!"
   }
   return response
 }
+
+
