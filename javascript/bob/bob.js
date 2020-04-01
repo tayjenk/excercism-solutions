cfdfc766d36d237fcb227806e807c068ada30241
Tayla Monet <taymjenk@gmail.com>
2020-04-01 13:23:28 -0400

    final solution, trim() input message

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,22 +3,23 @@
 // This is only a SKELETON file for the 'Bob' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-//const isSilent = message => RegExp(/^\''|^\s+$/).test(message)
-const isSilent = message => {if(message.trim() === '') return !!message}
+
+const isSilent = message => {if(message === '') return !!message}
 const isYellingAsking = message => message === message.toUpperCase() && RegExp(/[A-Z]\?/).test(message)
-const isAsking = message => message.trimEnd().endsWith("?")
+const isAsking = message => message.endsWith("?")
 const isYelling = message => message === message.toUpperCase() && RegExp(/[A-Z]/).test(message)
 
 
 export const hey = message => {
-  switch (!!message) {
-    case isSilent(message):
+  const messageTrim = message.trim()
+  switch (!!messageTrim) {
+    case isSilent(messageTrim):
       return "Fine. Be that way!"
-    case isYellingAsking(message):
+    case isYellingAsking(messageTrim):
       return "Calm down, I know what I'm doing!"
-    case isAsking(message):
+    case isAsking(messageTrim):
       return "Sure."
-    case isYelling(message):
+    case isYelling(messageTrim):
       return "Whoa, chill out!"
     default:
       return "Whatever."
