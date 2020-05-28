90fceea54f7416ac1d3b93f30493bd6942652492
Tayla Monet <taymjenk@gmail.com>
2020-05-28 12:33:08 -0400

    refactor: remove else statement, new for loop

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -7,13 +7,9 @@ public class Hamming {
         if (!leftStrand.isEmpty() && rightStrand.isEmpty()) throw new IllegalArgumentException("right strand must not be empty.");
         if (leftStrand.length() != rightStrand.length()) {
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
-        } else {
-            short i = 0;
-            int length = leftStrand.length();
-            while (i < length) {
-                if (leftStrand.charAt(i) != rightStrand.charAt(i)) this.hammingDist += 1;
-                i++;
-            }
+        }
+        for(short i=0; i < leftStrand.length(); i++) {
+            if (leftStrand.charAt(i) != rightStrand.charAt(i)) this.hammingDist += 1;
         }
     }
 
