2532cca2f192a5515e41e4e9cc2fb9bd3f66cdfb
Tayla Monet <taymjenk@gmail.com>
2020-06-05 17:57:33 -0400

    add: triangle solution

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -5,20 +5,32 @@
 
 export class Triangle {
   constructor(side1, side2, side3) {
-    this.side1 = side1,
-    this.side2 = side2,
-    this.side3 = side3
+    this.s1 = side1,
+    this.s2 = side2,
+    this.s3 = side3,
+    this.sidesSet = new Set([this.s1, this.s2, this.s3])
+    this.isIsosceles = this.isIsosceles.bind(this)
   }
 
   isEquilateral() {
-    return side1 === side2 && side2 === side3
+    if (!this.isValidTriangle()) return false
+    if (!this.s1 || !this.s2 || !this.s3) return false
+    return this.sidesSet.size === 1
   }
 
   isIsosceles() {
-    throw new Error("Remove this statement and implement this function");
+    if(!this.isValidTriangle()) return false
+    return this.sidesSet.size < 3
   }
 
   isScalene() {
-    throw new Error("Remove this statement and implement this function");
+    if(!this.isValidTriangle()) return false
+    return this.sidesSet.size === 3
+  }
+
+  isValidTriangle() {
+    if (!this.s1 || !this.s2 || !this.s3) return false
+    if(this.s1 + this.s2 < this.s3 || this.s1 + this.s3 < this.s2 || this.s2 + this.s3 < this.s1) return false
+    return true
   }
 }
